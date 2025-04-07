ce1c5897583c869e52425f419ba44dbf6061999c
Andrei Ivan <andrei.ivan1208@gmail.com>
2025-04-07 18:00:12 +0300

    fix: Specify entry HTML file for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/digital-art-gallery/', 
@@ -15,6 +16,11 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist' 
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html')
+      }
+    }
   }
 });

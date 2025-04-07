8d8cf763ce8683e4a19526a5ee4bfe40ea3bb174
Andrei Ivan <andrei.ivan1208@gmail.com>
2025-04-07 21:26:39 +0300

    fix: Import galleryComponent into main.js to ensure Alpine registration

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import '../css/styles.css'; // Import the main stylesheet
+import './alpine/galleryComponent.js'; // Import the component definition file
 
 import Alpine from '../../node_modules/alpinejs/dist/module.esm.js'
 
@@ -6,12 +7,7 @@ window.Alpine = Alpine
 
 // console.log("main.js: Setting up alpine:init listener.")
 
-// *** Corrected: Register data inside the 'alpine:init' event ***
-document.addEventListener('alpine:init', () => {
-    // console.log("main.js: alpine:init event fired. Registering gallery component.")
-})
 
-// Start Alpine AFTER setting up the listener
 Alpine.start()
 
 // console.log("main.js: Alpine.start() called.") 

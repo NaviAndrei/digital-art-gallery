3c5391a3db72465c356940b1be1976f5c08904b6
Andrei Ivan <andrei.ivan1208@gmail.com>
2025-04-07 19:33:57 +0300

    fix(vite): Use relative path for build input

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import { resolve } from 'path';
 
 export default defineConfig({
   base: '/digital-art-gallery/', 
@@ -19,7 +18,7 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/index.html')
+        main: 'src/index.html'
       }
     }
   }
